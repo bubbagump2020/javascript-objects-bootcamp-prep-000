1534c79773bbfd1eb7b49f63bec1115e8bfefb06
Kevin Bagnall <bubbagump2020@gmail.com>
2019-07-14 03:13:13 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,3 +3,7 @@ var playlist = {lord_huron: 'Brother'}
 function updatePlaylist(playlist, artistName, songTitle){
   return Object.assign(playlist, {[artistName]: 'songTitle'})
 }
+
+function removeFromPlaylist(playlist, artistName){
+  delete
+}
